df7eff85e6de3d8af0495ef271669a1e38b60c3c
Shania <shaniasiew14@gmail.com>
2026-02-03 21:30:51 -0400

    Domain methods- Shania

---
+++ App/models/redeemed_reward.py
@@ -0,0 +1,30 @@
+from App.database import db
+from datetime import datetime
+
+class RedeemedReward(db.Model):
+    __tablename__ ='redeemed_reward'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('student.id'), nullable=False)
+    reward_id = db.Column(db.Integer, db.ForeignKey('reward.id'), nullable=False)
+    redeemed_at = db.Column(db.DateTime, default=datetime.now, nullable=False)
+
+    def get_json(self):
+        return {
+            'id': self.id,
+            'studentId': self.student_id,
+            'reward': self.reward.get_json(),
+            'redeemedAt': self.redeemed_at.isoformat()
+        }
+
+    def __repr__(self):
+        return f'<RedeemedReward Student:{self.student_id} Reward:{self.reward_id}>'
+
+    def get_redeemed_at(self):
+        return self.redeemed_at
+    
+    def get_reward_id(self):
+        return self.reward_id
+    
+    def get_student_id(self):
+        return self.student_id
+    
