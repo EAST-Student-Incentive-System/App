3e08471d4fb53760b9ab5cde9d14d7ff1df5b3e6
Tyler-Baksh <tylerbaksh163@gmail.com>
2026-02-06 18:05:18 -0400

    Add viewBadges Badges controller

--- App/controllers/badge.py
+++ App/controllers/badge.py
@@ -2,6 +2,7 @@ from App.models import Badge
 from App.models import Student
 from App.database import db
 
+# Controller function to award a badge to a student
 def awardBadge(student_id, badge_id):
     student = Student.query.get(student_id)
     badge = Badge.query.get(badge_id)
@@ -12,3 +13,8 @@ def awardBadge(student_id, badge_id):
             db.session.commit()
             return True
     return False
+
+# Controller function to view all badges in the system
+def viewBadges():
+    badges = Badge.query.all()
+    return [badge.get_json() for badge in badges]
