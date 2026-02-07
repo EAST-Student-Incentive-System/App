1095ebc547e496e0646c2e22653af74cf16b0a14
Shania <shaniasiew14@gmail.com>
2026-02-06 21:26:16 -0400

    Updated Student Methods

--- App/models/student.py
+++ App/models/student.py
@@ -3,13 +3,16 @@ from .user import User
 from .attendance import Attendance
 from .redeemed_reward import RedeemedReward
 from .student_badge import StudentBadge
+from App.models.reward import Reward
 
 
 class Student(User):
     __tablename__ = 'student'
 
-    id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
-    points = db.Column(db.Integer, default=0, nullable=False)
+    
+    total_points = db.Column(db.Integer, default=0, nullable=False)
+    redeemed_points = db.Column(db.Integer, default=0, nullable=False)
+    current_balance = db.Column(db.Integer, default=0, nullable=False)
 
     attendances = db.relationship('Attendance', back_populates='student', cascade="all, delete-orphan")
     redeemed_rewards = db.relationship('Reward', secondary=RedeemedReward.__table__, backref='redeeming_students')
@@ -20,41 +23,40 @@ class Student(User):
     }
 
     def add_points(self, amount):
-        self.points += amount
+        if amount <= 0:
+            return
+        self.total_points += int(amount)
+        self.current_balance += int(amount)
 
     def subtract_points(self, amount):
-        if self.points >= amount:
-            self.points -= amount
+        if amount <= 0:
+            return False
+        if self.current_balance >= amount:
+            amt = int(amount)
+            self.redeemed_points += amt
+            self.current_balance -= amt
             return True
         return False
 
     def get_json(self):
         data = super().get_json()
         data.update({
-            'points': self.points,
+            'total_points': self.total_points,
+            'redeemed_points': self.redeemed_points,
+            'current_balance': self.current_balance,
             'badges': [badge.get_json() for badge in self.badges]
         })
         return data
 
     def __repr__(self):
-        return f'<Student {self.username}> - {self.points} points'
-
-    
-    def check_enough_points(self, amount):
-        return self.points >= amount
-    
-    def add_points(self, amount):
-        self.points += amount
-
-    def subtract_points(self, amount):
-        if self.points >= amount:
-            self.points -= amount
-            return True
-        return False
+        return f'<Student {self.username}> - {self.current_balance} points'
     
     def calculate_leaderboad_ranking(self):
-        students = Student.query.order_by(Student.points.desc()).all()
+        students = Student.query.order_by(Student.total_points.desc()).all()
         return students.index(self) + 1
+    
+    def check_enough_points(self, reward):
+        return self.current_balance >= reward.pointCost
     
 
     
