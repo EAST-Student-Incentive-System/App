7089957eb3c4b4530021a08bc608e689f751ead1
Shania <shaniasiew14@gmail.com>
2026-02-25 23:43:51 -0400

    Minor fix to signUp view

--- App/views/auth.py
+++ App/views/auth.py
@@ -4,6 +4,8 @@ from flask_jwt_extended import jwt_required, current_user, unset_jwt_cookies, se
 from App.database import db
 from App.models import Student, Staff
 from.index import index_views
+from App.controllers.auth import (
+    signUp, login, logout, change_password, setup_jwt       )
 
 from App.controllers import (
     login, create_user, get_all_users_json, get_user,
@@ -28,14 +30,12 @@ def identify_page():
 def signup_page():
     if request.method == 'POST':
         data = request.form
-        try:
-            user = create_user(data['email'], data['username'], data['password'])
-            flash(f"Account created for {user.username} as {user.role}")
+        result = signUp(data['email'], data['username'], data['password'])
+        if 'error' in result:
+            flash(result['error'])
+        else:
+            flash(f"Account created for {result['user']['username']} as {result['user']['role']}")
             return redirect(url_for('index_views.index_page'))
-        except ValueError as e:
-            flash(str(e))
-        except Exception as e:
-            flash("Error creating account: " + str(e))
     return render_template('signup.html', title="Sign Up")
 
 
