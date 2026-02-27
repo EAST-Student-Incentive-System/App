e2c204e9b894261118b11e33064f2d973a6c1c21
EmilyJugmohan1 <emilyjugmohan123@gmail.com>
2026-02-26 22:38:19 -0400

    login html edits

--- App/views/__init__.py
+++ App/views/__init__.py
@@ -5,16 +5,12 @@ from .index import index_views
 from .auth import auth_views
 from .badge import badge_views
 from .admin import setup_admin
-<<<<<<< HEAD
 from .reward import reward_views
 
 
 # Add new reward_views blueprint to the views list
 views = [user_views, index_views, auth_views, reward_views] 
-=======
-from .progress import progress_views
-
-
-views = [user_views, index_views, auth_views, badge_views] 
->>>>>>> 86b4225667b1cbf188246510f595ca4f21b826fa
 # blueprints must be added to this list
+
+from App.controllers.user import user
+views = [user]
