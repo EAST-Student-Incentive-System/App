48c1a2ca92ba93ccf1badd1c6bc7cfda2d8c3774
Shania <shaniasiew14@gmail.com>
2026-03-11 01:55:19 -0400

    Minor fixes to resolve wgsi error

--- App/controllers/redeemedReward.py
+++ App/controllers/redeemedReward.py
@@ -1,10 +1,10 @@
-from App.models import Reward, Student
+from App.models import Reward, Student, RedeemedReward
 from App.database import db
 
 
 def view_redeemed_rewards(student_id):
     student = Student.query.get(student_id)
-
     if not student:
         return None
-    return student.redeemed_rewards
+    # Return ORM objects directly
+    return RedeemedReward.query.filter_by(student_id=student_id).all()
