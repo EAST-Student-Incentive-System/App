ec912888e4bec4410c18215a8a92095f3438bc17
xXn00bslayerXx <adam4mohammed@gmail.com>
2026-03-16 15:05:03 -0400

    first draft of the staff_flagged page that works

--- App/views/user.py
+++ App/views/user.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, jsonify, request, send_from_directory, flash, redirect, url_for
 from flask_jwt_extended import jwt_required, current_user as jwt_current_user, get_jwt_identity
 from App.models import Student
+from App.models.staff import Staff
 
 from.index import index_views
 
@@ -34,7 +35,7 @@ def get_users_action():
 @user_views.route('/api/users', methods=['POST'])
 def create_user_endpoint():
     data = request.json
-    user = create_user(data['username'], data['password'])
+    user = create_user(data['username'], data['password']) # pyright: ignore[reportOptionalSubscript]
     return jsonify({'message': f"user {user.username} created with id {user.id}"})
 
 @user_views.route('/static/users', methods=['GET'])
@@ -63,3 +64,17 @@ def student_history_page(student_id):
     if history is None:
         return "Student not found", 404
     return render_template('student_history.html', history=history, user=user)
+
+# --------------------------------------------------
+# Staff command endpoints
+# --------------------------------------------------
+
+@user_views.route('/staff/flagged', methods=['GET'])
+@jwt_required()
+def flagged_command():   
+    user_id = get_jwt_identity()
+    user = Staff.query.get(user_id)
+    print (user_id, user)
+    if not user or not user.role == "staff":
+        return jsonify({'error': 'Unauthorized'}), 403
+    return render_template('staff_flagged.html', user=user)
