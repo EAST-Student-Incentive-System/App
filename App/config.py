708f9016c2c32f8fa297a2d925eed7c73e4b3cb1
Shania <shaniasiew14@gmail.com>
2026-04-03 16:33:31 -0400

    Added environment check

--- App/config.py
+++ App/config.py
@@ -5,11 +5,13 @@ from flask import flash, redirect, url_for, jsonify
 import cloudinary
 import cloudinary.uploader
 def init_cloudinary():
+    if os.environ.get("USE_CLOUDINARY", "false").lower() == "true":
         cloudinary.config(
-            cloud_name = os.environ.get("CLOUDINARY_CLOUD_NAME"),
-            api_key = os.environ.get("CLOUDINARY_API_KEY"),
-            api_secret = os.environ.get("CLOUDINARY_API_SECRET")
+            cloud_name=os.environ.get("CLOUDINARY_CLOUD_NAME"),
+            api_key=os.environ.get("CLOUDINARY_API_KEY"),
+            api_secret=os.environ.get("CLOUDINARY_API_SECRET")
         )
+
 
 
 def load_config(app, overrides):
